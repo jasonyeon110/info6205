30a2950f24148ed8bc73f84f5db6b45760421ccd
JasonYeon <jasonyeon110@gmail.com>
2021-02-04 22:28:37 -0800

    HW1 update

--- src/HW1/FindLeaves.java
+++ src/HW1/FindLeaves.java
@@ -1,16 +1,35 @@
 package HW1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindLeaves {
 
-//    public List<List<Integer>> findLeaves(TreeNode root){
-//        if(root == null){
-//            return new ArrayList<List<Integer>>();
-//        }
-//
-//        List<List<Integer>> leftNode = findLeaves(root.left);
-//        List<List<Integer>> rightNode = findLeaves(root.right);
-//
-//
-//    }
+    public List<List<Integer>> findLeaves(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return null;
+        }
+        //get the bottom leaves and add to the ArrayList
+        getLeaves(result, root);
+
+        return result;
+    }
+
+    private int getLeaves(List<List<Integer>> result, TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int lHeight = getLeaves(result, root);
+        int rHeight = getLeaves(result, root);
+        int height = Math.max(lHeight, rHeight) + 1;
+
+        if(result.size() <= height){
+            result.add(new ArrayList<>());
+        }
+        result.get(height).add(root.val);
+        return height;
+    }
 
 }

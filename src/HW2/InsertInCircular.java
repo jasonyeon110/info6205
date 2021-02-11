04fb9572651dc898174e1a3673ee0121d09fe396
JasonYeon <jasonyeon110@gmail.com>
2021-02-10 22:24:32 -0800

    update last two parts --Still on going--

---
+++ src/HW2/InsertInCircular.java
@@ -0,0 +1,7 @@
+package HW2;
+
+import ClassNotes.Feb04.Node;
+
+public class InsertInCircular {
+    public
+}
--- src/HW2/OddEven.java
+++ src/HW2/OddEven.java
@@ -3,18 +3,29 @@ package HW2;
 public class OddEven {
     public ListNode oddEvenList(ListNode head){
         if(head == null || head.next == null){
-            return null;
-        }
-        ListNode temp;
-
-        while (head.next != null){
-
-            temp = head.next;
-            head = head.next;
+            return head;
         }
 
-        oddEvenList(head.next);
+        ListNode headOdd = head;
+        ListNode headEven = headOdd.next;
+        ListNode odd = headOdd;
+        ListNode even = headEven;
 
-        return
+        while (even != null){
+            if(even.next != null){
+                odd.next = even.next;
+            } else {
+                odd.next = headEven;
+                return headOdd;
+            }
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+
+        }
+
+        odd.next = headEven;
+        return headOdd;
+
     }
 }

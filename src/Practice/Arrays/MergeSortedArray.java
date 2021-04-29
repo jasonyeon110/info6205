c7a066754d93e37ef8df5947434a4b85e803bf43
JasonYeon <jasonyeon110@gmail.com>
2021-04-28 23:17:29 -0700

    update

--- src/Practice/Arrays/MergeSortedArray.java
+++ src/Practice/Arrays/MergeSortedArray.java
@@ -4,10 +4,8 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = m; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                nums1[i] = nums2[j];
-            }
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
         }
         Arrays.sort(nums1);
         System.out.println(Arrays.toString(nums1));

1f98b42a27bee0639fdaf8a6321570efb6701cdc
jyeon <jasonyeon110@gmail.com>
2021-06-11 14:30:55 -0700

    update

--- src/Practice/RomanToInt.java
+++ src/Practice/RomanToInt.java
@@ -1,41 +1,34 @@
 package Practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInt {
     public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
         int res = 0;
 
-        if(s != null){
-            for (int i = 0; i < s.length()-1; i++) {
-                if( getValue(s.charAt(i)) >= getValue(s.charAt(i + 1))){
-                    res += getValue(s.charAt(i));
-                }
+        for (int i = 0; i < s.length() - 1; i++) {
+            if(map.get(s.charAt(i)) >= map.get(s.charAt(i + 1))){
+                int a = map.get(s.charAt(i));
+                res += a;
+            } else {
+             res  += (map.get(s.charAt(i+1)) - map.get(s.charAt(i)));
             }
+            res += map.get(s.charAt(s.length()-1));
         }
-    return res;
-    }
 
-    private static int getValue(char c){
-        switch (c){
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
-        }
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
     }
 }
